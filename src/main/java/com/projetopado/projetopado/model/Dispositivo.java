d365709b0bf5bbc4bcdc7bf0e6d45f3aef1bb148
MisaelMachado1998 <misaelmacielmachado@gmail.com>
2022-04-16 14:11:53 -0300

    Correção ID

--- src/main/java/com/projetopado/projetopado/model/Dispositivo.java
+++ src/main/java/com/projetopado/projetopado/model/Dispositivo.java
@@ -18,20 +18,10 @@ public class Dispositivo implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "deviceId")
-    private Long id;
+    private Long deviceId;
     private String name;
     private String mac;
     private String email;
     private Integer latitude;
     private Integer longitude;
-
-    public Dispositivo(Long id, String mac) {
-        this.id = id;
-        this.name = name;
-        this.mac = mac;
-        this.email = email;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
 }
